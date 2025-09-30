c953fb1362f066d68fc2a9a86b3fa00372075271
jennysitap <jennycasti.mens07@gmail.com>
2025-09-30 15:36:00 -0700

    Insertar registro

--- bloggg/app/Http/Controllers/UsersController.php
+++ bloggg/app/Http/Controllers/UsersController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
-
+use Illuminate\Support\Facades\Hash;
 class UsersController extends Controller
 {
     public function getUsers(){
@@ -13,7 +13,27 @@ class UsersController extends Controller
         return view("admin.users")
         ->with('usuarios',$data);
     }
-    public function createUsers(){
-        dd("Ya prendio tu");
+    public function createUsers(Request $request){
+        //dd($request->email);
+        $request->validate([
+            "name"=>'required|min:3',
+            "nickname"=>'required|min:3|unique:users,nickname',
+            "email"=>'required|email|unique:users,email',
+            "password"=>'required|min:8',
+            "password2"=>'required|min:8|same:password',
+        ]);
+        //guardar registro
+        $user=new User ();
+        $user->name=$request->name;
+        $user->nickname=$request->nickname;
+        $user->password=Hash::make($request->password);
+        $user->email=$request->email;
+        $user->img="default.jpg";
+        $user->save();
+        //dd("Usuario insertado");
+        return redirect()
+        ->back()
+        ->with('success',"Usuario insertado correctamente");
+
     }
 }
--- bloggg/resources/views/admin/users.blade.php
+++ bloggg/resources/views/admin/users.blade.php
@@ -1,18 +1,32 @@
-        @extends('admin.layouts.main')
-        @section('contenido')
-            <div class="d-flex justify-content-between">
-                <h1>
-                Usuarios
-                </h1>
-                <div>
-                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-                    Agregar
-                    </button>
-                </div>
-            </div>
-            
-            <div class="p-4">
-            <table class="table table-borderless">
+@extends('admin.layouts.main')
+@section('contenido')
+<div class="d-flex justify-content-between">
+    <h1>
+    Usuarios
+    </h1>
+    <div>
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+    Agregar
+    </button>
+    </div>
+</div>
+@if(session('success'))
+    <div class="alert alert-success">
+        {{session('success')}}
+    </div>
+@endif           
+@if($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach($errors->all() as $error)
+            <li>{{$error}}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
+<div class="p-4">
+    <table class="table table-borderless">
         <thead>
             <tr>
             <th scope="col">#</th>
@@ -25,26 +39,26 @@
             </tr>
         </thead>
         <tbody>
-                @foreach($usuarios as $item)
-                    <tr>
-                        <td>{{$item->id}}</td>
-                        <td>{{$item->imagen}}</td>
-                        <td>{{$item->name}}</td>
-                        <td>{{$item->nickname}}</td>
-                        <td>{{$item->email}}</td>
-                        <td>**********</td>
-                        <td>
-                            <button class="btn btn-danger">X</button>
-                        </td>
-                    </tr>
-                @endforeach
+            @foreach($usuarios as $item)
+                <tr>
+                    <td>{{$item->id}}</td>
+                    <td>{{$item->imagen}}</td>
+                    <td>{{$item->name}}</td>
+                    <td>{{$item->nickname}}</td>
+                    <td>{{$item->email}}</td>
+                    <td>**********</td>
+                    <td>
+                    <button class="btn btn-danger">X</button>
+                    </td>
+                </tr>
+            @endforeach
         </tbody>
-        </table>
-        </div>
-        <!-- Modal -->
-        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
+    </table>
+</div>
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLabel">Agregar Usuario</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
@@ -54,38 +68,44 @@
             <form action="/dashboard/users" method="POST">
                 @csrf
                 <div class="modal-body">
-                <div class="form-group">        
-                    <div class="modal-footer">
+                    <div class="form-group">
+                    <label for="name">Nombre</label>
+                    <input value="{{old('name')}}" name="name" type="text" class="form-control" id="nombre" aria-describedby="emailHelp">
+                </div>
+                <div class="form-group">
+                    <label for="nickname">Nickname</label>
+                    <input value="{{old('nickname')}}" name="nickname" type="text" class="form-control" id="nickname" aria-describedby="emailHelp">
+                </div>
+                <div class="form-group">
+                <label for="email">Correo</label>
+                <input value="{{old('email')}}" name="email" type="email" class="form-control" id="address" aria-describedby="emailHelp">
+                </div>
+                <div class="form-group">
+                <label for="password">Contraseña</label>
+                <input value="{{old('password')}}" name="password"type="password" class="form-control" id="password" aria-describedby="emailHelp">
+                </div>
+                <div class="form-group">
+                <label for="password">Confirmar Contraseña</label>
+                <input name="password2" type="password" class="form-control" id="password2" aria-describedby="emailHelp">
+                </div>
+                <div class="form-group">
+                <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
-            </div></form>
-            
-            <label for="name">Nombre</label>
-            <input type="text" class="form-control" id="nombre" aria-describedby="emailHelp">
+                <button type="submit" class="btn btn-primary">Save changes</button>
+                </div>
+            </form> 
         </div>
-        <div class="form-group">
-            <label for="nickname">Nickname</label>
-            <input type="text" class="form-control" id="nickname" aria-describedby="emailHelp">
-        </div><div class="form-group">
-            <label for="email">Correo</label>
-            <input type="email" class="form-control" id="address" aria-describedby="emailHelp">
-        </div>
-        <div class="form-group">
-            <label for="password">Contraseña</label>
-            <input type="password" class="form-control" id="password" aria-describedby="emailHelp">
-        </div>
-        <div class="form-group">
-            <label for="password">Confirmar Contraseña</label>
-            <input type="password" class="form-control" id="password2" aria-describedby="emailHelp">
-        </div>
-            </div>
-
-            </div>
-        </div>
-        </div>
-        @endsection
-        @section('scripts')
-            <script>
-                //alert("Hola")
-            </script>
-        @endsection
+    </div>
+</div>
+@endsection
+@section('scripts')
+<script>
+    //alert("Hola")
+    $(document).ready(function(){
+        $(".btnEliminar").on('click',function(event){
+            var id=$(this).data('id')
+            alert(id)
+        });
+    });
+</script>
+@endsection
